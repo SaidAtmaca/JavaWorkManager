4585e0f137b5b6f7fd9e35fa733b1c94c798777d
Said Atmaca <said.atmaca@bma.local>
2022-07-25 08:56:26 +0300

    Chaining and getWorkInfoByLiveData were added.

--- app/src/main/java/com/said/javaworkmanager/MainActivity.java
+++ app/src/main/java/com/said/javaworkmanager/MainActivity.java
@@ -1,11 +1,13 @@
 package com.said.javaworkmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
@@ -44,5 +46,40 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         WorkManager.getInstance(this).enqueue(workRequest);
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(workRequest.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                if(workInfo.getState()== WorkInfo.State.RUNNING){
+                    System.out.println("Running");
+                }else if(workInfo.getState()== WorkInfo.State.SUCCEEDED){
+                    System.out.println("Succeed");
+                }else if(workInfo.getState()== WorkInfo.State.FAILED){
+                    System.out.println("Failed");
+                }
+            }
+        });
+
+        //WorkManager.getInstance(this).cancelAllWork();
+
+
+        //Chainig
+
+        /*
+
+        OneTimeWorkRequest oneTimeWorkRequest=new OneTimeWorkRequest.Builder(RefreshDatabase.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build();
+        WorkManager.getInstance(this).beginWith(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .enqueue();
+
+         */
+
+
+
     }
 }
