6d11e13dbf78f504886aebab2c2f118115251781
Said Atmaca <said.atmaca@bma.local>
2022-07-25 07:52:01 +0300

    A class was created which extends Worker Class

---
+++ app/src/main/java/com/said/javaworkmanager/RefreshDatabase.java
@@ -0,0 +1,37 @@
+package com.said.javaworkmanager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class RefreshDatabase extends Worker {
+
+    Context myContext;
+
+    public RefreshDatabase(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.myContext=context;
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+
+        refreshdatabase();
+
+
+        return Result.success();
+    }
+
+
+    private void refreshdatabase(){
+        SharedPreferences sharedPreferences= myContext.getSharedPreferences("com.said.javaworkmanager",Context.MODE_PRIVATE);
+        int mySavedNumber= sharedPreferences.getInt("myNumber",0);
+        mySavedNumber= mySavedNumber+1;
+        System.out.println(mySavedNumber);
+        sharedPreferences.edit().putInt("myNumber",mySavedNumber).apply();
+    }
+}
